e298cf9a81503ab74c92316e7e089dcff4dcc142
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-24 00:32:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const codes = document.querySelectorAll('.code');
 
-codes[0].focus();
-code.forEach(( code , index ) => {
+window.onload = () => {
+	codes[0].focus();
+}
+
+codes.forEach(( code , index ) => {
 	code.addEventListener('input' , (e) =>  {
 	if( e.target.value && index < codes.length - 1 ) {
 		codes[index + 1].focus();
