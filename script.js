a338f9f5c98e4f9fb6c87d41a0d65219f892e3e6
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-24 00:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+code.forEach(( code , index ) => {
+	code.addEventListener('input' , (e) =>  {
+	if( e.target.value && index < codes.length - 1 ) {
+		codes[index + 1].focus();
+	}
+});
+
+code.addEventListener('keydown' , (e) => {
+	if(e.key === 'Backspace') {
+		code.value = '';
+		if(index > 0) {
+			codes[index - 1].focus();
+		}
+	}
+});
+});
